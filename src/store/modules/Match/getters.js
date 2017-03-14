0cad04f01abb230a38d31024bedb7f9c5d8f2991
unknown <wujiang@SLWY.COM>
2017-03-14 18:27:52 +0800

    　按比赛已进行时间降序排序

--- src/store/modules/Match/getters.js
+++ src/store/modules/Match/getters.js
@@ -1,14 +1,41 @@
 export default {
     live: state => {
-        if (!state.live.length) return state.live
+        if (!state.live.length) {
+            return state.live
+        }
         const liveObj = {}
+        const liveArr = []
+        //  相同联赛的组合在一起
         state.live.forEach((item, index) => {
-            const leagueID = item.league && item.league.i
-            if (!leagueID) return
-            if (!liveObj[leagueID]) liveObj[leagueID] = { league: {}, matches: [] }
-            liveObj[leagueID].league = item.league
-            liveObj[leagueID].matches.push(item)
+                const leagueID = item.league && item.league.i
+                if (!leagueID) {
+                    return
+                }
+                if (!liveObj[leagueID]) { 
+                    liveObj[leagueID] = {
+                        league: {},
+                        matches: []
+                    }
+                }
+                liveObj[leagueID].league = item.league
+                liveObj[leagueID].matches.push(item)
+            })
+
+        for (let key in liveObj) {
+            liveArr.push(liveObj[key])
+        }
+
+        //  转换成数组后按比赛已进行时间降序排序
+        liveArr.sort((cur, next) => {
+            var curMaxStatus = Math.max.apply(null, cur.matches.map(item => {
+                    return item.events_graph.status
+                }))
+            var nextMaxStatus = Math.max.apply(null, next.matches.map(item => {
+                    return item.events_graph.status
+                }))
+            return nextMaxStatus - curMaxStatus
         })
-        return liveObj
+
+        return liveArr
     }
 }
