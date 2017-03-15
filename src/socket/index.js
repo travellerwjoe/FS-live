2165988e186ce6a271ac714e9dce7ae2ed750213
joe.wu <traveller_wjoe@sina.com>
2017-03-15 23:50:19 +0800

    修改socket连接地址，因使用localhost会导致在其它设备上访问时访问的是其设备本机

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 import config from '@/config'
 
-const socket = io(config.server, {
+const socket = io(`:${config.port}/`, {
     reconnection: true
 })
 export default socket
