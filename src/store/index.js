18018dbfbe4faea16cf1799b5645aa9661de8553
unknown <wujiang@SLWY.COM>
2017-03-20 22:16:43 +0800

    更改vuex结构

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,23 @@ import Vuex from 'vuex'
 import * as actions from './actions'
 import * as getters from './getters'
 import Live from './modules/Live'
+import { ADD_TO_GOALING_QUEUE } from './mutation-types'
 
 Vue.use(Vuex)
 
+const state = {
+    goalingQueue: []
+}
+
+const mutations = {
+    // 添加到进球信息到进球队列
+    [ADD_TO_GOALING_QUEUE](state, item) {
+        state.goalingQueue.push(item)
+    }
+}
+
 const store = new Vuex.Store({
+    state,
     actions,
     getters,
     modules: {
--- src/store/modules/Live/index.js
+++ src/store/modules/Live/index.js
@@ -3,17 +3,12 @@ import getters from './getters'
 import { FETCH_LIVE, ADD_TO_GOALING_QUEUE } from '@/store/mutation-types'
 
 const state = {
-    live: [],
-    goalingQueue: []
+    live: []
 }
 
 const mutations = {
     [FETCH_LIVE](state, live) {
         state.live = live
-    },
-    // 添加到进球信息到进球队列
-    [ADD_TO_GOALING_QUEUE](state, item) {
-        state.goalingQueue.push(item)
     }
 }
 
